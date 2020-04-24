4a93bb301dc2f8c18a8c55dc8207b841d45039f8
openjanala <openjanala@gmail.com>
2020-04-24 10:46:39 +0600

    Rowter and switch is Completed

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>This is the 4o4 Not Founds</h1>
+        </div>
+    );
+};
+
+export default NotFound;
